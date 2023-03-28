124bf6e0d87d7c026e561b3da44f3571c84829e2
laurapascual <laurapascualmateo@gmail.com>
2023-03-28 13:08:33 +0200

    Add btn to create card

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,10 +1,14 @@
 import Profile from "../getAvatar/Profile";
 import Card from "./Card";
 import "../../styles/layouts/Preview.scss";
+import { Link } from "react-router-dom";
 
 const Preview = ({ data, cover }) => {
   return (
     <section className="preview">
+      <Link to='/' className="preview__link">
+      <button className="preview__btn">VER PROYECTOS</button>
+      </Link>
       <Profile avatar={data.photo} className="image"></Profile>
       <Card data={data}></Card>
     </section>
